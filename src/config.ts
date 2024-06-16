4148d42344397258d0edbf9df76532244376a89d
ayou001 <1601432788@qq.com>
2024-06-16 20:21:58 +0800

    more

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
 	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
 	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
 	homePage: 'https://weekly.tw93.fun',
-	blogPage: 'https://tw93.fun/',
-
+	blogPage: 'https://blog-about-tian.vercel.app/',
+	morePage: 'https://bento.me/ayou011',
 	githubId: 'ayou001',
 	repo: 'ayou001/weekly',
 };
