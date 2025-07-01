55eb359929f151f97daace18f631998af3e39ee8
ayou001 <1601432788@qq.com>
2025-07-01 21:27:50 +0800

    001_1

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-	title: '友谊博客',
+	title: '二战博客',
 	author: 'ayou001',
-	description: '记录泽与朋友的友谊，努力成为热爱生活的人~',
+	description: '记录泽与朋友的二战生活，努力成为热爱生活的人~',
 	keywords: 'ayou001,Weekly,Blog,前端,MiaoYan,Pake,Swift,设计',
 	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
 	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
